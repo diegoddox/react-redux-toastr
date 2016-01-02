67986021851cf79808e3ffc7b660fa352fa45a0d
Diego Oliveira <diegoddox@gmail.com>
2016-01-02 12:04:35 +0100

    change names

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const ADD_TOASTR     = '@ReduxToastr/ADD_TOASTR';
-export const REMOVE_TOASTR  = '@ReduxToastr/REMOVE_TOASTR';
-export const CLEAN_TOASTR   = '@ReduxToastr/CLEAN_TOASTR';
-export const SHOW_CONFIRM   = '@ReduxToastr/SHOW_CONFIRM';
-export const HIDE_CONFIRM   = '@ReduxToastr/HIDE_CONFIRM';
+export const ADD_TOASTR     = '@ReduxToastr/toastr/ADD';
+export const REMOVE_TOASTR  = '@ReduxToastr/toastr/REMOVE';
+export const CLEAN_TOASTR   = '@ReduxToastr/toastr/CLEAN';
+export const SHOW_CONFIRM   = '@ReduxToastr/confirm/SHOW';
+export const HIDE_CONFIRM   = '@ReduxToastr/confirm/HIDE';
