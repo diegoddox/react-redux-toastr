755e6dcc02668b45cfea1a61ffa9e2389310940d
Diego Oliveira <diegoddox@gmail.com>
2016-01-26 11:15:01 +0100

    move web pack to a separated folder

---
+++ build/webpack/base.js
@@ -0,0 +1,29 @@
+'use strict';
+var webpack = require('webpack');
+
+module.exports = {
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  output: {
+    library: 'ReduxToastr',
+    libraryTarget: 'umd'
+  },
+  resolve: {
+    extensions: ['', '.js']
+  }
+};
