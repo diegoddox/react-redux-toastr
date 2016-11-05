8a5f08868797d0d6943119c2e0f258a72d016f0a
Diego Oliveira <diegoddox@gmail.com>
2016-11-05 14:24:30 +0100

    wp

--- development/Avatar.js
+++ development/Avatar.js
@@ -1,8 +1,22 @@
 import React from 'react';
+
+const styles = {
+  width: '70px',
+  height: '70px',
+  overflow: 'hidden',
+  borderRadius: '50%',
+  border: '2px solid white',
+  margin: 'auto'
+};
+
+const imgStyle = {
+  maxWidth: '105%'
+};
+
 export default () => {
   return (
-    <div className="toastr-avatar">
-      <img src="./assets/bob.jpg" />
+    <div className="toastr-avatar" style={styles}>
+      <img src="./assets/bob.jpg" style={imgStyle}/>
     </div>
   );
 };
