ca399a86a9767d9357f501a1db0497b9823f75b1
Diego Oliveira <diegoddox@gmail.com>
2017-04-10 17:20:10 +0200

    refactor unused styles

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = props => (
   <button type="button" onClick={() => props.onClick()} className={props.className}>
-    <p>{props.children}</p>
+    {props.children}
   </button>
 );
 
