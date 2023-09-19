8368c9f565f888e6a02f3c746d6eebc08063cc5d
Jeewaka Michael Fernando <Jeewakaphotography@gmail.com>
2023-09-20 02:05:54 +0530

    Update client.js

--- development/client.js
+++ development/client.js
@@ -1,10 +1,12 @@
 'use strict';
 import React from 'react';
-import {render} from 'react-dom';
 import createStore from './store';
 import App from './App';
+import ReactDOM from "react-dom/client";
 
 const store = createStore();
-const target = document.getElementById('app');
+const root = ReactDOM.createRoot(document.getElementById("app"));
 
-render(<App store={store} />, target);
+root.render(
+    <App store={store} />
+);
